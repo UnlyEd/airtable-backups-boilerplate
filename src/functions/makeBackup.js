ada341cb02a7f1f5df426e4ff71a196de6238fd2
Hugo Martin <hugo.martin@epitech.eu>
2019-10-17 14:30:07 +0100

    Check event before use it

--- src/functions/makeBackup.js
+++ src/functions/makeBackup.js
@@ -3,6 +3,11 @@ import {uploadBackup} from "../utils/backup";
 import Epsagon from "epsagon";
 
 export const handler = async (event, context, callback) => {
+    if (!event.hasOwnProperty('AIRTABLE_BASE') || !event.hasOwnProperty('AIRTABLE_TABLES') || !event.hasOwnProperty('S3_DIRECTORY')) {
+        const err = Error(`Can't access to AIRTABLE_BASE or AIRTABLE_TABLES or S3_DIRECTORY in the event variable`);
+        Epsagon.setError(err);
+        throw err;
+    }
     const tables = event["AIRTABLE_TABLES"].split(";");
     const airtableContent = await fetchDataFromAirtable(event, tables);
     try {
