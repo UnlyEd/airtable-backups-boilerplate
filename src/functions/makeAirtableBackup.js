ba917d12137472b153e2d9960a06c1632a205d8c
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2019-11-19 13:29:41 +0100

    Cleanup for OSS release
    Fix makeAirtableBackup handler path
    
    Improved s3 documentation
    
    Improved credentials documentation
    
    Fixed function to call
    
    Improved documentation
    
    Improved doc
    
    Clean serverless file of old functions
    
    Update doc about lifecycle
    
    Fix command invoke:airtableBackupsBoilerplate (both subcommands weren't using the NODE_ENV as expected)
    
    Update yarn.lock
    
    Cleanup (#4)
    
    * Remove .env.staging
    
    * Massive cleanup (doc + recommended usage)
    
    * Allow to select the storage class to use, per scheduled backup
    
    Misc typo
    
    Update dependencies (airtable security issue https://snyk.io/vuln/SNYK-JS-AIRTABLE-480404)

--- src/functions/makeAirtableBackup.js
+++ src/functions/makeAirtableBackup.js
@@ -8,12 +8,44 @@ export const handler = async (event, context, callback) => {
     Epsagon.setError(err);
     throw err;
   }
+
   const tables = event['AIRTABLE_TABLES'].split(';');
   const airtableContent = await fetchDataFromAirtable(event, tables);
+
   try {
-    await uploadBackup(event, airtableContent);
+    const response = await uploadBackup(event, airtableContent);
+
+    try {
+      const {
+        __metadata: {
+          Bucket: bucketName,
+          StorageClass: storageClass,
+          Key: backupName,
+        },
+      } = response;
+      console.log(`Backup "${backupName}" successfully uploaded to bucket "${bucketName}" (using storage class: "${storageClass}")`);
+
+      return {
+        statusCode: 200,
+        body: 'Successfully created backup',
+      };
+
+    } catch (e) {
+      console.error(e);
+      Epsagon.setError(e);
+
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          'error': e,
+          'humanReadableError': 'Error while uploading data to S3',
+        }),
+      };
+    }
   } catch (e) {
+    console.error(e);
     Epsagon.setError(e);
+
     return {
       statusCode: 500,
       body: JSON.stringify({
@@ -22,8 +54,4 @@ export const handler = async (event, context, callback) => {
       }),
     };
   }
-  return {
-    statusCode: 200,
-    body: 'Successfully created backup',
-  };
 };
--- src/utils/backup.js
+++ src/utils/backup.js
@@ -4,10 +4,22 @@ import moment from 'moment';
 export const uploadBackup = async (event, jsonData) => {
   const s3 = new AWS.S3();
   const BACKUP_NAME = event['S3_DIRECTORY'] + moment().format('YYYY_MM_DD_HH-mm-ss') + '.json';
-
-  return await s3.putObject({
+  const storageClass = event['STORAGE_CLASS'] || 'STANDARD_IA'; // "STANDARD"|"REDUCED_REDUNDANCY"|"STANDARD_IA"|"ONEZONE_IA"|"INTELLIGENT_TIERING"|"GLACIER"|"DEEP_ARCHIVE"
+  const params = {
     Bucket: process.env.S3_BUCKET,
+    StorageClass: storageClass,
     Key: BACKUP_NAME,
+  };
+
+  const response = await s3.putObject({
+    ...params,
     Body: JSON.stringify(jsonData),
   }).promise();
+
+  return {
+    ...response,
+    __metadata: {
+      ...params,
+    },
+  };
 };
